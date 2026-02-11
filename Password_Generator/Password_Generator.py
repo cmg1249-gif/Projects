1c5235e56926ab99b57c73b9585d2c5a1beb535f
cmg1249-gif <cmg1249@tm.edu>
2026-02-11 14:45:02 -0600

    Add password generator script
    This script generates a random password based on user-defined criteria including letters, symbols, and numbers.

---
+++ Password_Generator/Password_Generator.py
@@ -0,0 +1,22 @@
+import random
+letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+print("Welcome to the PyPassword Generator!")
+nr_letters = int(input("How many letters would you like in your password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+
+chosen_letters = random.sample(letters, nr_letters)
+
+chosen_symbols = random.sample(symbols, nr_symbols)
+chosen_numbers = random.sample(numbers, nr_numbers)
+chosen_letters2 = "".join(chosen_letters)
+chosen_symbols2 = "".join(chosen_symbols)
+chosen_numbers2 = "".join(chosen_numbers)
+
+pre_password = chosen_letters2 + chosen_symbols2 + chosen_numbers2
+new_password = "".join(random.sample(pre_password, len(pre_password)))
+print(new_password)
