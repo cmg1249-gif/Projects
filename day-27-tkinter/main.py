2d45082ee946006c63a55e616e907eadfc6c67fc
cmg1249 <cmg1249@tm.edu>
2026-07-11 23:10:29 -0500

    another quick update to the repo

---
+++ day-27-tkinter/main.py
@@ -0,0 +1,29 @@
+# import tkinter
+#
+# window = tkinter.Tk()
+# window.title("My first GUI Program")
+# window.minsize(500, 300)
+#
+#
+# #Label
+#
+# my_label = tkinter.Label(text="I am A Label", font=("Arial", 25, "bold"))
+# my_label.pack(expand=True)
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# window.mainloop()
+
+def  add(*args):
+    print(sum(args))
+
+add(1, 2, 3, 4, 5)
